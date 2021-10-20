1e12da8f15836c03e05ea6988175f49fbfc22b81
Araceli Valdovinos <valdovinosaraceli50@gmail.com>
2021-10-20 14:00:46 -0500

    fetched marketSchedule data with useEffect

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,4 +1,23 @@
+// import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
+import { useEffect, useState } from 'react';
+
 const Home = () => {
+
+  const [schedule, setSchedule] = useState(null)
+
+  useEffect(()=>{
+    fetch('http://localhost:8000/marketSchedule')
+      .then((response) =>{
+        return response.json();
+      })
+      .then((data)=>{
+        // console.log(data)
+        setSchedule(data);
+      })
+  }, [])
+
+
   return ( 
     <div className="home">
       <h2>Welcome</h2>
